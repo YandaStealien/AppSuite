4fe5b0eea98fb1b44a81939ade21e8d6514d030c
YandaStealien <abdullah@stealien.com>
2025-03-12 11:35:07 +0700

    Add files via upload
    AddProject

---
+++ AppSuite/AppSuite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppSuite
+//
+//  Created by stealien on 3/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var imgHome: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imgHome.layer.cornerRadius = 10
+        // Do any additional setup after loading the view.
+    }
+}
+
---
+++ AppSuite/Pods/Target Support Files/Pods-AppSuite/Pods-AppSuite-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppSuite : NSObject
+@end
+@implementation PodsDummy_Pods_AppSuite
+@end
